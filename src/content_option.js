b3bcc12e7f2e32b279f668b865d00ebe2e298715
Irtaza Hussain <irtaza@Irtazas-MacBook-Air.local>
2024-06-21 18:58:11 +0500

    chore: code-push

---
+++ src/content_option.js
@@ -0,0 +1,177 @@
+const logotext = "IRTAZA";
+
+const meta = {
+  title: "Irtaza Hussain",
+  description:
+    "I’m Irtaza Hussain software engineer _ Full stack devloper,currently working in Pakistan",
+};
+
+const introdata = {
+  title: "I’m Irtaza Hussain",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+    fourth: "I design systems",
+  },
+  description:
+    "Transforming Ideas into Interactive Digital Experiences with Cutting-Edge Web Development",
+  your_img_url: "https://i.ibb.co/BGmnPFh/IMG-20240430-WA0012.jpg",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "Nayatel's Principal Creative Executive, with a Bachelor's degree in Computer Science from the University of Azad Jammu and Kashmir, I thrive on crafting cutting-edge web applications that prioritize performance and accessibility. At the helm of innovative projects, my focus has been on developing applications that not only meet, but exceed SEO standards, are lightweight, fast, and fully optimized for progressive web experiences. My expertise lies in Android Framework and React Hooks, enabling me to create intuitive and user-friendly interfaces. With certifications in Critical Thinking and Digital Marketing, I support our team to deliver applications that resonate with our users and align with Nayatel's vision. Leveraging React JS skills, we are committed to enhancing user engagement and driving technological advancement.",
+};
+
+const worktimeline = [
+  {
+    jobtitle: "Android Developer",
+    where: "SolFreaks",
+    designation: "Internship",
+    date: "2020",
+  },
+  {
+    jobtitle: "Software Engineer",
+    where: "Nayatel",
+    designation: "FullTime",
+    date: "2022",
+  },
+];
+
+const skills = [
+  {
+    name: "JavaScript",
+    value: 90,
+  },
+  {
+    name: "Node/Express",
+    value: 90,
+  },
+  {
+    name: "React/Next - JS",
+    value: 90,
+  },
+  {
+    name: "Swift - AutoLayout",
+    value: 80,
+  },
+  {
+    name: "PHP",
+    value: 80,
+  },
+  {
+    name: "Linux Container - Lxd/Lxc",
+    value: 100,
+  },
+  {
+    name: "Redis",
+    value: 100,
+  },
+  {
+    name: "RabbitMQ",
+    value: 100,
+  },
+  {
+    name: "Zabbix/Grafanna/Prometheus/Uptime-Kuma",
+    value: 100,
+  },
+];
+
+const services = [
+  {
+    title: "iOS Apps",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Web Apps",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "API Developement",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Cache Integration",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Messaging queue Integration",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "Our streaming application, built with React JS, allows you to easily consume data from YouTube's rapid api v3, available on the RAPID API Platform...",
+    link: "https://yt-media-iota.vercel.app/",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "A rate-limiting algorithm is used to check if the user session (or IP address) is following the limits set within the server/network configuration.",
+    link: "https://insight-stack.vercel.app/api/blog",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "A rate-limiting algorithm is used to check if the user session (or IP address) is following the limits set within the server/network configuration.",
+    link: "https://insight-stack.vercel.app/api/blog",
+  },
+  {
+    img: "https://picsum.photos/500/?grayscale",
+    description:
+      "Proud to contribute as a Backend Developer at Naya TV! 🚀 Enhanced our Android app with Adaptive Bitrate Streaming (ABR) for top-notch viewing...",
+    link: "https://nayatv.nayatel.com/",
+  },
+  {
+    img: "https://picsum.photos/500/400/?grayscale",
+    description:
+      "This powerful CMS allows users to manage content for Android TVs with ease. From uploading and editing content, to scheduling its display...",
+    link: "https://signage.nayatel.com/",
+  },
+  {
+    img: "https://picsum.photos/600/400/?grayscale",
+    description:
+      "The app allows users to search for movies and view their details. I used the TMDB APIs...",
+    link: "https://movies-liard-sigma.vercel.app/",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "irtazawani100@gmail.com",
+  YOUR_FONE: "+92-340-086-658",
+  description:
+    "I value meaningful connections and collaborations. Whether you're interested in discussing a project, exploring a partnership opportunity, or simply want to say hello, I'd love to hear from you. Drop me a message using the form below, and I'll get back to you as soon as possible. Let's create something great together! ",
+  YOUR_SERVICE_ID: "service_s5qevjc",
+  YOUR_TEMPLATE_ID: "template_4e8hil2",
+  YOUR_USER_ID: "kSxjtE_LS0W3a94Z2",
+};
+
+const socialprofils = {
+  github: "https://github.com/irtaza9",
+  facebook: "https://www.facebook.com/irtaza.hussainwani/",
+  linkedin: "https://www.linkedin.com/in/irtaza-hussain/",
+  twitter: "https://x.com/IrtazaHussain_",
+};
+
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
